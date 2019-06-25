b859413f61b1671bfcb040c69045fe71ce00b566
Jamal Hashim <jah649@cornell.edu>
2019-06-24 20:36:22 -0700

    router

---
+++ frontend/src/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Contact extends React.Component {
+    render() {
+      return (
+    <div>
+      <h1>hi</h1>
+    </div>
+        );
+    }
+  }
+
+export default Contact;
---
+++ frontend/src/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class HomePage extends React.Component {
+    render() {
+      return (
+    <div>
+      <h1>pls take our survey pls</h1>
+      <a href = "/contact">router test</a>
+    </div>
+        );
+    }
+  }
+
+export default HomePage;
