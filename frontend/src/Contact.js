cc23656e1eeb3061be7588d644388902cc59e748
ssc255 <ssc255@cornell.edu>
2019-06-25 00:26:31 -0400

    nonsense edits to homepage

--- frontend/src/Contact.js
+++ frontend/src/Contact.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 class Contact extends React.Component {
-    render() {
-      return (
-    <div>
-      <h1>hi</h1>
-    </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>hi</h1>
+      </div>
+    );
   }
+}
 
 export default Contact;
