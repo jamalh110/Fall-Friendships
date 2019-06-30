b4dd651eb3c7bd60818b2499c1e00fd245576c57
ssc255 <ssc255@cornell.edu>
2019-06-30 03:17:31 -0400

    more fixes on the nav

--- frontend/src/Components/About/About.js
+++ frontend/src/Components/About/About.js
@@ -1,27 +1,32 @@
 import React from "react";
 import Navbar from "../NavigationBar/NavigationBar";
-import { GoogleLogin } from 'react-google-login';
-import App from "../../App.js"
-import State from "../../State.js"
+import { GoogleLogin } from "react-google-login";
+import App from "../../App.js";
+import State from "../../State.js";
+import classes from "./About.module.css";
 
 class About extends React.Component {
-  componentWillMount(){
+  componentWillMount() {
     //console.log(this.props)
-    if(this.props.location.state == null){
+    if (this.props.location.state == null) {
       this.setState({
-        state: (new State())
-      })
-    } else{
+        state: new State()
+      });
+    } else {
       this.setState({
         state: this.props.location.state.state
-      })
+      });
     }
   }
-  
+
   render() {
     return (
-      <div>
-        <Navbar history = {this.props.history} state = {this.state.state} googleCallBack = {App.googleResponseSuccess.bind(this)}/>
+      <div className={classes.mainAbout}>
+        <Navbar
+          history={this.props.history}
+          state={this.state.state}
+          googleCallBack={App.googleResponseSuccess.bind(this)}
+        />
         <h1>About</h1>
       </div>
     );
