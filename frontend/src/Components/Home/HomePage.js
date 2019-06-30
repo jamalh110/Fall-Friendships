428598f0daef4596fa777be2cad79baf1336bbec
ssc255 <ssc255@cornell.edu>
2019-06-30 03:45:05 -0400

    home edit

--- frontend/src/Components/Home/HomePage.js
+++ frontend/src/Components/Home/HomePage.js
@@ -56,9 +56,19 @@ class HomePage extends React.Component {
           state={this.state.state}
           googleCallBack={App.googleResponseSuccess.bind(this)}
         />
-        <h1>Fall Friendships</h1>
-        <h3>insert tagline</h3>
-        <p>description</p>
+        <div className={classes.homeTexts}>
+          <h1>Fall Friendships</h1>
+          <h3>Start the year off right. Meet someone new. Find your people!</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
         <br />
         <br />
         {decision}
