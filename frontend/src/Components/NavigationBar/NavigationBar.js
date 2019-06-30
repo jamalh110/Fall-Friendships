1e21a21d5de5302aee31129c8d9b012a12ed21a4
ssc255 <ssc255@cornell.edu>
2019-06-30 02:35:14 -0400

    navbar looks sexxxx

--- frontend/src/Components/NavigationBar/NavigationBar.js
+++ frontend/src/Components/NavigationBar/NavigationBar.js
@@ -6,6 +6,7 @@ import classes from "./NavigationBar.module.css";
 import App from "../../App.js";
 import browserHistory from "react-router";
 import gloginimage from "../../Assets/web/2x/btn_google_signin_light_normal_web@2x.png";
+import fflogo from "../../Assets/yeet.png";
 
 class NavigationBar extends React.Component {
   componentWillMount() {
@@ -22,34 +23,42 @@ class NavigationBar extends React.Component {
       return (
         <div>
           <Navbar className={classes.MainNavbar} expand="md">
-            <Navbar.Brand href="#">THC x CBA | FF Logo</Navbar.Brand>
+            <Navbar.Brand href="#">
+              <div className={classes.brandBox}>
+                <img className={classes.fflogo} src={fflogo} />
+                <p>thc x cba</p>
+              </div>
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav" className={classes.NavLinks}>
               <Nav>
                 <Nav.Link
+                  className={classes.nlink}
                   onClick={function() {
                     this.props.history.push("/", { state: this.props.state });
                   }.bind(this)}
                 >
-                  Home
+                  <p>Home</p>
                 </Nav.Link>
                 <Nav.Link
+                  className={classes.nlink}
                   onClick={function() {
                     this.props.history.push("/about", {
                       state: this.props.state
                     });
                   }.bind(this)}
                 >
-                  About
+                  <p>About</p>
                 </Nav.Link>
                 <Nav.Link
+                  className={classes.nlink}
                   onClick={function() {
                     this.props.history.push("/contact", {
                       state: this.props.state
                     });
                   }.bind(this)}
                 >
-                  Contact
+                  <p>Contact</p>
                 </Nav.Link>
                 <GoogleLogin
                   clientId="29949178420-0opvqqshb6ltbdmhceqgcout83b7s5i2.apps.googleusercontent.com"
