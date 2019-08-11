036f485004fdb90a5d204305064732baac86abc9
Jamal Hashim <jah649@cornell.edu>
2019-08-11 05:04:40 -0700

    survey debug

--- frontend/src/Components/Survey/Survey.js
+++ frontend/src/Components/Survey/Survey.js
@@ -18,7 +18,9 @@ class SurveyClass extends React.Component {
       })
     })
   }
-
+ componentWillMount(){
+   console.log(this.props.state.state)
+ }
   //code to be run when survey is submitted 
   onCompleteFollowup = function (result) {
     fetch(App.backendURL + "submit_survey", {
